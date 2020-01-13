6edf8530e69fba93ad33736ebc0e08a328890de1
Isak Olsson <isaak.olsson@gmail.com>
2020-01-13 14:23:53 +0100

    Leaderboards working for each category

--- src/components/Leaderboards/index.js
+++ src/components/Leaderboards/index.js
@@ -1,66 +1,70 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Leaderboards.css";
-import { Row, Col, Card, Spin } from "antd";
+import { Row, Col, Card, Skeleton, Alert } from "antd";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { compose } from "redux";
 import { firebaseConnect, isLoaded, isEmpty } from "react-redux-firebase";
+import { get_categories } from "../../redux/actions/categories";
 
+const mapStateToProps = state => {
+  return {
+    top10: state.firebaseReducer.ordered.top10,
+    topOfCats: state.firebaseReducer.ordered.topOfCats,
+    auth: state.firebaseReducer.auth,
+    profile: state.firebaseReducer.profile,
+    cats: state.categoryReducer.categories,
+    categoryStatus: state.categoryReducer.status,
+    categoryMessage: state.categoryReducer.message
+  };
+};
 
-const cats = [{id: 9, name: "General Knowledge"},{id: 10, name: "Entertainment: Books"},{id: 11, name: "Entertainment: Film"},
-{id: 12, name: "Entertainment: Music"},{id: 13, name: "Entertainment: Musicals & Theatres"},{id: 14, name: "Entertainment: Television"},
-{id: 15, name: "Entertainment: Video Games"},{id: 16, name: "Entertainment: Board Games"},{id: 17, name: "Science & Nature"},
-{id: 18, name: "Science: Computers"},{id: 19, name: "Science: Mathematics"},{id: 20, name: "Mythology"},{id: 21, name: "Sports"},
-{id: 22, name: "Geography"},{id: 23, name: "History"},{id: 24, name: "Politics"},{id: 25, name: "Art"},{id: 26, name: "Celebrities"},
-{id: 27, name: "Animals"},{id: 28, name: "Vehicles"},{id: 29, name: "Entertainment: Comics"},{id: 30, name: "Science: Gadgets"},
-{id: 31, name: "Entertainment: Japanese Anime & Manga"},{id: 32, name: "Entertainment: Cartoon & Animations"}];
-  
+const mapDispatchToProps = dispatch => {
+  return {
+    get_categories: () => dispatch(get_categories())
+  };
+};
 
 const enhance = compose(
   firebaseConnect(props => [
     {
       path: "users",
       storeAs: "top10",
-      queryParams: ["orderByChild=points", "limitToLast=10"]
+      queryParams: ["orderByChild=points", "limitToLast=15"]
     },
     {
-      path: "leaderboards",
-      storeAs: "topOfCats",
-      queryParams: ["orderByValue"]
+      path: "leaderboards/categories",
+      storeAs: "topOfCats"
     }
   ]),
-  connect(state => ({
-    top10: state.firebaseReducer.ordered.top10,
-    topOfCats: state.firebaseReducer.ordered.topOfCats,
-    profile: state.firebaseReducer.profile
-  }))
+  connect(mapStateToProps, mapDispatchToProps)
 );
 
 const Leaderboards = props => {
+  //if categories has not been loaded yet from the API this session, do it
+  useEffect(() => {
+    if (!props.cats.length > 0) props.get_categories();
+  }, []);
+
   return (
-    <Row gutter={15}>
+    <Row gutter={20}>
+
+      {props.categoryStatus === "error" && <Alert type="error" message={props.categoryMessage} />}
+
       <Col xs={24} md={12} lg={8} xl={6}>
-        <Card className="top10" title=" Top 10 players of all time">
+        <Card className="top10" title=" Top 15 players of all time">
           <div className="leaderboard-card-body">
             {!isLoaded(props.top10) ? (
-              <Spin />
+              <Skeleton active />
             ) : !isEmpty(props.top10) ? (
               [...props.top10].reverse().map((player, i) => {
                 return (
-                  <div className={"displayName" in player.value && player.value.displayName == props.profile.displayName ? "a-player you" : "a-player"} key={i}>
+                  <div className={player.key === props.auth.uid ? "a-player you" : "a-player"} key={i}>
                     <div className="player-no">{i + 1}.</div>
                     <div className="player-name">
-                      {"displayName" in player.value && player.value.displayName == props.profile.displayName
-                        ? (player.value.displayName + " (you)")
-                        : "displayName" in player.value ? player.value.displayName
-                        : player.key}
-                      {i == 0 ? (
-                        <span> &#129351;</span>
-                      ) : i == 1 ? (
-                        <span> &#129352;</span>
-                      ) : i == 2 ? (
-                        <span> &#129353;</span>
-                      ) : null}
+                      {"displayName" in player.value ? player.value.displayName : player.key}
+                      {player.key === props.auth.uid && " (you)"}
+                      {i == 0 ? <span> &#129351;</span> : i == 1 ? <span> &#129352;</span> : i == 2 ? <span> &#129353;</span> : null}
                     </div>
                     <div>{player.value.points}</div>
                   </div>
@@ -72,41 +76,57 @@ const Leaderboards = props => {
           </div>
         </Card>
       </Col>
-      {cats.map((cat, i) => {
-        return (
-          <Col key={i} xs={24} md={12} lg={8} xl={6}>
-            <Card
-              title={cat.name}
-              extra={<Link to={"/new-quiz/" + cat.id}>Take quiz</Link>}
-            >
-              <div className="leaderboard-card-body">
-                {!isLoaded(props.topOfCats) ? (
-                  <Spin />
-                ) : !isEmpty(props.topOfCats) ? (
-                  [...props.topOfCats].find(e => e.value == cat.id).map((player, i) => {
-                //cat.players.map((player, i) => {
-                  return (
-                    <div className="a-player" key={i}>
-                      <div className="player-no">{i + 1}.</div>
-                      <div className="player-name">
-                        {player.value.Object.key.displayName}
-                        {i == 0 ? (
-                          <span> &#129351;</span>
-                        ) : i == 1 ? (
-                          <span> &#129352;</span>
-                        ) : i == 2 ? (
-                          <span> &#129353;</span>
-                        ) : null}
-                      </div>
+
+      <Col className="leaderboard-right" xs={24} md={12} lg={16} xl={18}>
+        <Row gutter={20}>
+          {typeof props.topOfCats !== "undefined" && props.cats.length > 0 && props.categoryStatus === "success" ? (
+            props.topOfCats.map((c, i) => {
+              let findCategory = props.cats.find(cat => cat.id == c.key);
+              return typeof findCategory !== null ? (
+                <Col key={i} xs={24} md={24} lg={12} xl={8}>
+                  <Card className="category-card" title={findCategory.name} extra={<Link to={"/new-quiz/" + c.key}>Take quiz</Link>}>
+                    <div className="leaderboard-card-body">
+                      {Object.keys(c.value)
+                        .sort((a, b) => c.value[b].points - c.value[a].points)
+                        .map((p, j) => {
+                          return (
+                            <div className={c.value[p].uid === props.auth.uid ? "a-player you" : "a-player"} key={j}>
+                              <div className="player-no">{j + 1}.</div>
+                              <div className="player-name">
+                                {c.value[p].displayName}
+                                {c.value[p].uid === props.auth.uid && " (you)"}
+                                {j == 0 ? <span> &#129351;</span> : j == 1 ? <span> &#129352;</span> : j == 2 ? <span> &#129353;</span> : null}
+                              </div>
+                              <div>{c.value[p].points}</div>
+                            </div>
+                          );
+                        })}
                     </div>
-                  );
-                })
-              ): null}
-              </div>
-            </Card>
-          </Col>
-        );
-      })}
+                  </Card>
+                </Col>
+              ) : null;
+            })
+          ) : (
+            <React.Fragment>
+              <Col xs={24} md={24} lg={12} xl={8}>
+                <Card>
+                  <Skeleton active />
+                </Card>
+              </Col>
+              <Col xs={24} md={24} lg={12} xl={8}>
+                <Card>
+                  <Skeleton active />
+                </Card>
+              </Col>
+              <Col xs={24} md={24} lg={12} xl={8}>
+                <Card>
+                  <Skeleton active />
+                </Card>
+              </Col>
+            </React.Fragment>
+          )}
+        </Row>
+      </Col>
     </Row>
   );
 };
